1f02307ec5d49db79bd2970759676f74ebaf26ba
Ouattara Mamadou <ouattara.mamadou@sepaq.com>
2016-11-29 11:25:55 -0500

    first commit

---
+++ src/main/java/com/madouat/test/DemoMadouatApplication.java
@@ -0,0 +1,12 @@
+package com.madouat.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMadouatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoMadouatApplication.class, args);
+  }
+}
