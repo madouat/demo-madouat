57208e4248b2e58edd5596c41d8858b6e917bb1d
Ouattara Mamadou <ouattara.mamadou@sepaq.com>
2016-12-01 15:50:33 -0500

    admin server

--- src/main/java/com/madouat/app/Application.java
+++ src/main/java/com/madouat/app/Application.java
@@ -3,9 +3,11 @@ package com.madouat.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
 // @EnableZuulProxy
 @SpringBootApplication
-// @EnableAdminServer
+@EnableAdminServer
 public class Application {
 
   public static void main(String[] args) {
