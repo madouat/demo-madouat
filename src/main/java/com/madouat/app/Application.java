acb52bf2f8af1b3f4a225905a87f880caa9054d1
Ouattara Mamadou <ouattara.mamadou@sepaq.com>
2016-12-01 15:14:34 -0500

    disable admin server

--- src/main/java/com/madouat/app/Application.java
+++ src/main/java/com/madouat/app/Application.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
-
 @EnableZuulProxy
 @SpringBootApplication
-@EnableAdminServer
+// @EnableAdminServer
 public class Application {
 
   public static void main(String[] args) {
