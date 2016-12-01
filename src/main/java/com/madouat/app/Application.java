b0488c4b7c84191e962f765297c19ea7eb4e02c9
Ouattara Mamadou <ouattara.mamadou@sepaq.com>
2016-12-01 15:05:53 -0500

    activated zuul proxy

--- src/main/java/com/madouat/app/Application.java
+++ src/main/java/com/madouat/app/Application.java
@@ -2,8 +2,13 @@ package com.madouat.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@EnableZuulProxy
 @SpringBootApplication
+@EnableAdminServer
 public class Application {
 
   public static void main(String[] args) {
