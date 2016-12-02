83b59b40d08bd34813643cdecaa12e301e5ada05
Ouattara Mamadou <ouattara.mamadou@sepaq.com>
2016-12-02 11:29:38 -0500

    to war

--- src/main/java/com/madouat/app/Application.java
+++ src/main/java/com/madouat/app/Application.java
@@ -2,12 +2,13 @@ package com.madouat.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
 // @EnableAdminServer
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
